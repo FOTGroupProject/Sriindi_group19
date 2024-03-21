4092612cff61c0cad7810a196d2d8bb20342f943
yohan vishvajith <yohanvishvajith@gmail.com>
2024-03-21 10:17:42 +0530

    blacklist add
    add to blacklist function added

---
+++ database/migrations/2024_03_01_182030_reviews.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('author');
+            $table->bigInteger('product_id');
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->integer('rating');
+            $table->text('review');
+            $table->date('submit_date');
+            $table->timestamps();
+            // Add other columns as needed
+
+            // Primary Key
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\product;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/orders', function () {
+    return view('orders');
+});
+Route::get('/product', function () {
+    return view('product');
+});
+Route::get('/profile', function () {
+    return view('profile');
+});
+Route::get('/customers', function () {
+    return view('customers');
+});
+Route::get('/addblacklist', [product::class, 'store'])->name('addtoblacklist');
+
+
